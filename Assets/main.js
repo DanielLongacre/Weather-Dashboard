06dcc51a98ce77552daa4d5ec60ea8d7043e1840
Daniel Longacre <danieljlongacre@gmail.com>
2023-08-04 17:07:10 -0400

    Added function to get City information from API call using fetch

--- Assets/main.js
+++ Assets/main.js
@@ -4,9 +4,36 @@
 $(function() {
 
 
-//Global Variables
+var getCityData = function (city) {
+    var city = $('#userInput').val();
+    var APIKey = '6fda8e05fd463ff807e270df95f92c8d';
+    var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+
+
+    fetch(queryURL)
+        .then(function (response) {
+            if(response.ok) {
+                response.json().then(function(data) {
+                    displayWeather(data, city);
+                });
+            } else {
+                alert.apply('Error: ' + response.statusText);
+            }
+        })
+        .catch(function(error) {
+            alert('Unable to connect to Weather Geocoding API');
+        });
+};
+
+function displayWeather() {
+    
+}
+
 var userInput = document.querySelector('#userInput');
-// var ulEl = document.querySelector('#searchedCities');
+
+//Get the current date
+var dateToday = dayjs();
+$('#today').text(dateToday.format('D/MM/YYYY'));
 
 
 //Array of searched cities
@@ -15,6 +42,10 @@ var previousCities = [];
 
 $('.btn-primary').on('click', function() {
     var value = $('#userInput').val();
+
+    getCityData(value);
+    console.log(getCityData(value));
+
     previousCities.push(value);
     localStorage.setItem('Cities', previousCities);
 
