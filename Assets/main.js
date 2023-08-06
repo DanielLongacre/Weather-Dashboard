386eddfe524a9e92541a95c65f62cb01d27790c5
Daniel Longacre <danieljlongacre@gmail.com>
2023-08-06 09:43:26 -0400

    Created function to make buttons for previously searched for cities

--- Assets/main.js
+++ Assets/main.js
@@ -11,7 +11,7 @@ function pastCities() {
     var saved_cities = JSON.parse(localStorage.getItem('cities'));
 
     if(saved_cities !== null) {
-        cities = saved_cities;
+        cities = saved_cities
     }
 
     
@@ -27,6 +27,30 @@ function saveCity() {
 var city = $('#userInput').val();
 var APIKey = '6fda8e05fd463ff807e270df95f92c8d';
 var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+
+
+
+//Get previously searched for cities and create buttons for them
+function prevCityBtns() {
+    pastCities.innerHTML = '';
+    if(cities == null) {
+        return;
+    }
+    var diff_cities = [...new Set(cities)];
+    for(var i = 0; i < diff_cities.length; i++) {
+        var cityName = diff_cities[i];
+
+        var btnEl = document.createElement('button');
+        btnEl.textContent = cityName;
+        btnEl.setAttribute('class', 'listBtn');
+
+        pastCities.appendChild(btnEl);
+        
+    }
+
+
+}
+
 
 
 
